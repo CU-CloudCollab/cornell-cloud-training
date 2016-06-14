73c9e4b8dac3c1cedb9a3adec5bab968b0c6edc6
Scott Ross <sr523@cornell.edu>
2016-06-14 00:02:47 -0400

    Delete testplugin.rb
    test plugin .. no need to be on master

--- _plugins/testplugin.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  class TestPlugin < Liquid::Tag
-    def render(context)
-      "It's Working!"
-    end
-  end
-end
-
-Liquid::Template.register_tag('testplugin', Jekyll::TestPlugin)
